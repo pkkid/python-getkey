cccde4402873844d58593cc19936a819864934ca
Michael Shepanski <mshepanski@nasuni.com>
2024-06-19 14:45:02 -0400

    Bump Version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='getkeys',
-    version='1.0.1',
+    version='1.0.2',
     description='Micro-package to get keys from a list of paths',
     author='Michael Shepanski',
     author_email='michael.shepanski@gmail.com',
